ea20b8cb70df3f410a735ba7afe77cd558d08979
unknown <bberlin@BorisPC.jomax.paholdings.com>
2018-07-26 17:18:48 -0700

    DE-215 added data fetching

---
+++ ESOM_DataProc/ESOM_DataProc/MT_SalesOrders.sql
@@ -0,0 +1,18 @@
+select
+	top 10
+	/*ds not null*/
+	orderid,  
+	/* nullable  ????  what to do?  */
+	startdate,  
+	/*ds not null*/
+	account,  
+	/*ds not null*/
+	country,  
+	/*ds not null*/
+	ordertypeid, 
+	/*ds not null*/
+	tax, 
+	/*ds not null*/
+	total 
+from [dbo].[vAllocationDetail]
+	where ordertypeid in (1,2,3) and orderid > 0
